236406ec0e76123d0337a2faaf6d22db6b299f72
Nazar-Natin <114873430+Nazar-Natin@users.noreply.github.com>
2024-01-23 18:27:25 +0100

    Initial commit

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,12 @@
-let some:unknown;
-some = 'Text';
+let some: unknown;
+some = "Text";
+
 let str: string;
-str = some;
+
+if (typeof some === "string") {
+  str = some;
+} else {
+  console.error('Error: "some" is not a string.');
+}
 
 export {};
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -1,23 +1,23 @@
-/*
-  Ви маєте форму реєстрації користувачів. 
-  Іноді потрібно попередньо заповнити форму даними користувача для оновлення його профілю. 
-  Однак вам не завжди потрібно заповнити всі поля. Наприклад, користувач може хотіти оновити лише свій email та пароль, 
-  залишивши ім'я та прізвище без змін.
-
-  Виправте тип у аргументі функції так, щоб не було помилок типу.
-*/
-
 type User = {
   name: string;
   surname: string;
   email: string;
   password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User>): User {
+  const currentUser: User = {
+    name: initialValues.name || "DefaultName",
+    surname: initialValues.surname || "DefaultSurname",
+    email: initialValues.email || "DefaultEmail",
+    password: initialValues.password || "DefaultPassword",
+  };
+
+  return currentUser;
 }
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
-}
-
-createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
-
-export {};
+const updatedUser = createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
+console.log(updatedUser);
